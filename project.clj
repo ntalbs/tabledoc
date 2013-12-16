83f8a1f58a34d3c2b2424cad5f7b5ca67ff7c677
ntalbs <ntalbsen@gmail.com>
2013-12-16 10:05:13 +0900

    add project.clj

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject tabledoc "0.1.0"
+  :description "Oracle Schema documentation generator."
+  :license {:name "The MIT License (MIT)"
+            :url "http://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 ])
