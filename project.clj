ea0aab734d079c49e582e10645813b5753b71aff
ntalbs <ntalbsen@gmail.com>
2014-04-25 15:09:33 +0900

    Update dependencies.

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
 (defproject tabledoc "0.1.0"
   :description "Oracle Schema documentation generator."
-  :license {:name "The MIT License (MIT)"
-            :url "http://opensource.org/licenses/MIT"}
+  :license {:name "The MIT License (MIT)" :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojure-contrib "1.2.0"] 
+                 [org.clojure/java.jdbc "0.3.3"]
+                 [local/ojdbc6 "11.2.0.1"]
                  [enlive "1.1.5"]
-                 [org.clojars.zentrope/ojdbc "11.2.0.3.0"]
-                 [korma "0.3.0-RC6"]]
+                 [korma "0.3.1"]]
   :main tabledoc
 )
