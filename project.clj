0b8baee000149441862d0496c0c0c2dbdb8e4738
ntalbs <ntalbsen@gmail.com>
2014-04-28 18:34:00 +0900

    postgresql, mysql JDBC driver dependency 추가.

--- project.clj
+++ project.clj
@@ -5,6 +5,8 @@
                  [org.clojure/clojure-contrib "1.2.0"] 
                  [org.clojure/java.jdbc "0.3.3"]
                  [local/ojdbc6 "11.2.0.1"]
+                 [mysql/mysql-connector-java "5.1.30"]
+                 [org.postgresql/postgresql "9.3-1101-jdbc41"]
                  [enlive "1.1.5"]
                  [korma "0.3.1"]]
   :main tabledoc
